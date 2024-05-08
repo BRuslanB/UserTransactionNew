e1287e3d7c4138d53dfac90552e23f348091b45b
BRuslanB <rbaidyussenov@gmail.com>
2024-05-08 12:50:31 +0500

    modified entrypoint.sh

--- src/main/resources/cassandra/entrypoint.sh
+++ src/main/resources/cassandra/entrypoint.sh
@@ -8,9 +8,9 @@
 ## It is compatible with any cassandra:* image
 ##
 
+set -e
 
 ## Create script that executes files found in docker-entrypoint-initdb.d/
-
 cat <<'EOF' >> /run-init-scripts.sh
 #!/usr/bin/env bash
 
@@ -22,18 +22,18 @@ if [ -f "$LOCK" ]; then
     exit 0
 fi
 
-cd $INIT_DIR
+cd "$INIT_DIR"
 
 echo "@@ Executing bash scripts found in $INIT_DIR"
 
-# execute scripts found in INIT_DIR
+# Execute scripts found in INIT_DIR
 for f in $(find . -type f -name "*.sh" -executable -print | sort); do
     echo "$0: sourcing $f"
     . "$f"
     echo "$0: $f executed."
 done
 
-# wait for cassandra to be ready and execute cql in background
+# Wait for cassandra to be ready and execute cql in background
 (
     while ! cqlsh -e 'describe cluster' > /dev/null 2>&1; do sleep 6; done
     echo "$0: Cassandra cluster ready: executing cql scripts found in $INIT_DIR"
@@ -42,24 +42,60 @@ done
         cqlsh -f "$f"
         echo "$0: $f executed"
     done
-    # mark things as initialized (in case /var/lib/cassandra was mapped to a local folder)
-    touch $LOCK
+    # Mark things as initialized (in case /var/lib/cassandra was mapped to a local folder)
+    touch "$LOCK"
 ) &
 
 EOF
 
 ## Patch existing entrypoint to call our script in the background
-# This has been inspired by https://www.thetopsites.net/article/51594713.shtml
 EP=/patched-entrypoint.sh
-#sed '$ d' /docker-entrypoint.sh > $EP # can't work in ver.4
-cat <<'EOF' >> $EP
+cat <<'EOF' >> "$EP"
+#!/usr/bin/env bash
 /run-init-scripts.sh &
 exec "$@"
 EOF
 
-# Make both scripts executable
+## Make both scripts executable
 chmod +x /run-init-scripts.sh
-chmod +x $EP
+chmod +x "$EP"
 
-# Call the new entrypoint
-$EP "$@"
+CURRENT_IP=$(hostname -i)
+
+## Replace Cassandra settings in cassandra.yaml with values from environment variables, if they are set
+# Record 11 in cassandra.yaml
+sed -i "s/^ *cluster_name:/cluster_name:/" /etc/cassandra/cassandra.yaml
+
+if [ -n "$CASSANDRA_SEEDS" ]; then
+# Record 553 in cassandra.yaml
+    sed -i "s/^ *- seeds:.*/      - seeds: \"$CASSANDRA_SEEDS\"/" /etc/cassandra/cassandra.yaml
+else
+# CASSANDRA_SEEDS variable is not set or empty
+    sed -i "s/^ *- seeds:.*/      - seeds: \"$CURRENT_IP\"/" /etc/cassandra/cassandra.yaml
+fi
+
+if [ -n "$CASSANDRA_LISTEN_ADDRESS" ]; then
+# Record 763 in cassandra.yaml
+    sed -i -e "s/^listen_address: .*/listen_address: $CASSANDRA_LISTEN_ADDRESS/" /etc/cassandra/cassandra.yaml
+#    sed -i -e "s/^listen_address: .*/listen_address: $CURRENT_IP/" /etc/cassandra/cassandra.yaml
+fi
+
+if [ -n "$CASSANDRA_BROADCAST_ADDRESS" ]; then
+# Record 777 in cassandra.yaml
+    sed -i -e "s/^# broadcast_address: .*/broadcast_address: $CASSANDRA_BROADCAST_ADDRESS/" /etc/cassandra/cassandra.yaml
+#    sed -i -e "s/^# broadcast_address: .*/broadcast_address: $CURRENT_IP/" /etc/cassandra/cassandra.yaml
+fi
+
+if [ -n "$CASSANDRA_RPC_ADDRESS" ]; then
+# Record 859 in cassandra.yaml
+    sed -i -e "s/^rpc_address: .*/rpc_address: $CASSANDRA_RPC_ADDRESS/" /etc/cassandra/cassandra.yaml
+fi
+
+if [ -n "$CASSANDRA_BROADCAST_RPC_ADDRESS" ]; then
+# Record 875 in cassandra.yaml
+    sed -i -e "s/^# broadcast_rpc_address: .*/broadcast_rpc_address: $CASSANDRA_BROADCAST_RPC_ADDRESS/" /etc/cassandra/cassandra.yaml
+#    sed -i -e "s/^# broadcast_rpc_address: .*/broadcast_rpc_address: $CURRENT_IP/" /etc/cassandra/cassandra.yaml
+fi
+
+## Call the new entrypoint
+"$EP" "$@"
