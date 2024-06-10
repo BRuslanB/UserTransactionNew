ef2c8e518db071e855677ef35759e9f1681bceda
BRuslanB <rbaidyussenov@gmail.com>
2024-06-10 23:59:54 +0500

    fixed setting cassandra for test in dockers

--- docker_app/entrypoint.sh
+++ docker_app/entrypoint.sh
@@ -4,7 +4,8 @@
 ./wait-for-databases_ready.sh
 
 # Launch tests
-./gradlew test
+./gradlew clean
+./gradlew test -D spring.profiles.active=test
 
 # Change permissions back to original (e.g., 755)
 chmod -R 755 /app > /dev/null 2>&1
