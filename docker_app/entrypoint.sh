1885d23520228fff6110a9b4fdbf718c59252ea0
BRuslanB <rbaidyussenov@gmail.com>
2024-06-16 19:44:21 +0500

    Add CI workflow for Docker Compose project

--- docker_app/entrypoint.sh
+++ docker_app/entrypoint.sh
@@ -4,8 +4,8 @@
 ./wait-for-databases_ready.sh
 
 # Launch tests
-./gradlew clean
-./gradlew test -D spring.profiles.active=test
+# ./gradlew clean
+# ./gradlew test -D spring.profiles.active=test
 
 # Change permissions back to original (e.g., 755)
 chmod -R 755 /app > /dev/null 2>&1
